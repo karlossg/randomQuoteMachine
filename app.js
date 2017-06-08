1cfdb814f189dcd16e083f33b18ea245e02c03e8
Karol <karol.gratus@gmail.com>
2017-06-08 19:18:48 +0200

    Create app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var quote = '';
 var author = '';
 
 function getRandomQuote() {
-  fetch('https://talaikis.com/api/quotes/random/').then(function (response) {
+  fetch ('https://random-quote-generator.herokuapp.com/api/quotes/random').then(function (response) {
     if (response.status !== 200) {
       console.log('There was a problemm with response. Status Code: ' + response.status);
       return;
